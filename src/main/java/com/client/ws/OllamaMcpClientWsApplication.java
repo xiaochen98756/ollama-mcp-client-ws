6d915c7fcabfae2f71016455ae9913b964a6f762
wangxiaochen <1047800119@qq.com>
2025-10-14 17:12:10 +0800

    入口修改为controller

--- src/main/java/com/client/ws/OllamaMcpClientWsApplication.java
+++ src/main/java/com/client/ws/OllamaMcpClientWsApplication.java
@@ -1,6 +1,5 @@
 package com.client.ws;
 
-import com.client.ws.core.SpringWebSocketConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,7 +9,6 @@ public class OllamaMcpClientWsApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(OllamaMcpClientWsApplication.class, args);
-        SpringWebSocketConfigurator.setApplicationContext(context);
     }
 
 }
